efbae9fbfe01b521d5dd38f609304c2aa6ff82d1
Sora819 <sora.pisti@gmail.com>
2025-08-07 21:24:46 +0200

    Add items and recipes configs

--- src/main/java/me/sora819/specialitems/config/ConfigHandler.java
+++ src/main/java/me/sora819/specialitems/config/ConfigHandler.java
@@ -6,10 +6,14 @@ public class ConfigHandler {
     public static ConfigAdapter defaultConfig = new DefaultConfig();
     public static ConfigAdapter defaultMessagesConfig = new LocalizationConfig("en");
     public static ConfigAdapter messagesConfig = new LocalizationConfig(defaultConfig.get("language"));
+    public static ConfigAdapter recipesConfig = new CustomConfig("recipes.yml");
+    public static ConfigAdapter itemsConfig = new CustomConfig("items.yml");
 
     public static void reloadConfigurations() {
         defaultConfig.reload();
         defaultMessagesConfig.reload();
+        recipesConfig.reload();
+        itemsConfig.reload();
 
         messagesConfig = new LocalizationConfig(defaultConfig.get("language"));
         LocalizationHandler.messagesConfig = messagesConfig;
