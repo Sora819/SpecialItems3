6d4437509239021a64068a7901124327e182de9c
Pisti <sora.pisti@gmail.com>
2025-08-07 15:26:40 +0200

    Made flying boots functional

--- src/main/java/me/sora819/specialitems/items/FlyingBoots.java
+++ src/main/java/me/sora819/specialitems/items/FlyingBoots.java
@@ -1,17 +1,23 @@
 package me.sora819.specialitems.items;
 
+import me.sora819.specialitems.SpecialItems;
 import me.sora819.specialitems.localization.LocalizationHandler;
+import me.sora819.specialitems.utils.NBTManager;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockDispenseArmorEvent;
-import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.List;
 
-public class FlyingBoots implements ICustomItem {
+public class FlyingBoots implements ICustomItem, Listener {
 
     @Override
     public String getName() {
@@ -41,27 +47,43 @@ public class FlyingBoots implements ICustomItem {
 
         base_item.setItemMeta(meta);
 
+        NBTManager.setNBT(base_item, "custom_item", getID());
+
         return base_item;
     }
 
     @Override
     public boolean checkItem(ItemStack item) {
-        return false;
+        if (item == null) {
+            return false;
+        }
+
+        String custom_id = NBTManager.getNBT(item, "custom_item");
+
+        return custom_id != null && custom_id.equalsIgnoreCase(getID());
     }
 
+    @EventHandler
     public void onEquipArmorByInteract(PlayerInteractEvent e) {
-
+        e.getPlayer().setAllowFlight(checkItem(e.getPlayer().getInventory().getBoots()));
     }
 
+    @EventHandler
     public void onEquipArmorFromInventory(InventoryClickEvent e) {
-
+        Bukkit.getScheduler().runTask(SpecialItems.getInstance(), () -> {
+            Player player = (Player)e.getWhoClicked();
+            player.setAllowFlight(checkItem(player.getInventory().getBoots()));
+        });
     }
 
+    @EventHandler
     public void onEquipArmorFromDispenser(BlockDispenseArmorEvent e) {
-
+        Player player = (Player)e.getTargetEntity();
+        player.setAllowFlight(checkItem(player.getInventory().getBoots()));
     }
 
-    public void onPlayerDeath(PlayerDeathEvent e) {
-
+    @EventHandler
+    public void onPlayerDeath(PlayerRespawnEvent e) {
+        e.getPlayer().setAllowFlight(checkItem(e.getPlayer().getInventory().getBoots()));
     }
 }
