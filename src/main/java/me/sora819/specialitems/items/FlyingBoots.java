4e09a11c4d3f4a1614ac87e38704164e6ac54233
Sora819 <sora.pisti@gmail.com>
2025-08-08 12:15:27 +0200

    Don't disable flight in creative

--- src/main/java/me/sora819/specialitems/items/FlyingBoots.java
+++ src/main/java/me/sora819/specialitems/items/FlyingBoots.java
@@ -3,13 +3,16 @@ package me.sora819.specialitems.items;
 import me.sora819.specialitems.SpecialItems;
 import me.sora819.specialitems.utils.NBTManager;
 import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockDispenseArmorEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.PlayerGameModeChangeEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerItemBreakEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -41,6 +44,10 @@ public class FlyingBoots implements ICustomItem, Listener {
 
     @EventHandler
     public void onEquipArmorByInteract(PlayerInteractEvent e) {
+        if (e.getPlayer().getGameMode() == GameMode.CREATIVE) {
+            return;
+        }
+
         e.getPlayer().setAllowFlight(checkItem(e.getPlayer().getInventory().getBoots()));
     }
 
@@ -49,18 +56,31 @@ public class FlyingBoots implements ICustomItem, Listener {
         // We have to wait for the next tick, so that we can check the new state of the inventory
         Bukkit.getScheduler().runTask(SpecialItems.getInstance(), () -> {
             Player player = (Player)e.getWhoClicked();
+
+            if (player.getGameMode() == GameMode.CREATIVE) {
+                return;
+            }
+
             player.setAllowFlight(checkItem(player.getInventory().getBoots()));
         });
     }
 
     @EventHandler
     public void onEquipArmorFromDispenser(BlockDispenseArmorEvent e) {
+        if (e.getTargetEntity() instanceof Player && ((Player)e.getTargetEntity()).getGameMode() == GameMode.CREATIVE) {
+            return;
+        }
+
         Player player = (Player)e.getTargetEntity();
         player.setAllowFlight(checkItem(player.getInventory().getBoots()));
     }
 
     @EventHandler
     public void onPlayerDeath(PlayerRespawnEvent e) {
+        if (e.getPlayer().getGameMode() == GameMode.CREATIVE) {
+            return;
+        }
+
         e.getPlayer().setAllowFlight(checkItem(e.getPlayer().getInventory().getBoots()));
     }
 }
