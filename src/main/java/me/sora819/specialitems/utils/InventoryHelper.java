45ed18c65290972cbe93b4b2709a49db0ccd8f37
Sora819 <sora.pisti@gmail.com>
2025-10-31 11:19:09 +0100

    Improve InventoryHelper functions

--- src/main/java/me/sora819/specialitems/utils/InventoryHelper.java
+++ src/main/java/me/sora819/specialitems/utils/InventoryHelper.java
@@ -2,9 +2,10 @@ package me.sora819.specialitems.utils;
 
 import me.sora819.specialitems.items.ICustomItem;
 import me.sora819.specialitems.items.ItemRegistry;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.Arrays;
 
 public class InventoryHelper {
     public static boolean isItemPresent(String id, PlayerInventory inventory) {
@@ -13,7 +14,7 @@ public class InventoryHelper {
     }
 
     public static boolean isItemPresent(ICustomItem item, PlayerInventory inventory) {
-        return inventory.contains(item.getItemStack());
+        return Arrays.stream(inventory.getContents()).anyMatch(item::checkItem);
     }
 
     public static int getItemCount(String id, PlayerInventory inventory) {
@@ -22,14 +23,6 @@ public class InventoryHelper {
     }
 
     public static int getItemCount(ICustomItem item, PlayerInventory inventory) {
-        AtomicInteger count = new AtomicInteger();
-
-        inventory.forEach(itemStack -> {
-            if (itemStack != null && item.checkItem(itemStack)) {
-                count.set(count.get() + itemStack.getAmount());
-            }
-        });
-
-        return count.get();
+        return Arrays.stream(inventory.getContents()).filter(item::checkItem).mapToInt(ItemStack::getAmount).sum();
     }
 }
